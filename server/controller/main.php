feabb1f632b64cc95330439a8cf62afa2befd101
Iulian Peta <ipeta@isoftwaresystems.com>
2020-01-30 12:27:27 +0200

    add controller and main_v to server

---
+++ server/controller/main.php
@@ -0,0 +1,10 @@
+<?php 
+class main extends page
+{
+	function display()
+	{
+		$main_v=new main_v();
+		$main_v->display();
+	}
+}
+?>
---
+++ server/view/main_v.php
@@ -0,0 +1,25 @@
+<?php 
+final class main_v extends page_v
+{
+	function __construct()
+	{
+		parent::__construct("main","Home");
+	}
+	function headCustomCss()
+	{
+		
+	}
+	function headCustomScripts()
+	{
+	
+	}
+	function sideBar()
+	{
+	
+	}
+	function mainBar()
+	{
+	
+	}
+}
+?>
